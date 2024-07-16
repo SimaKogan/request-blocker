ece9d04e3506755c49e37c81fdfae58b6dc53812
SimaKogan <simas96@mail.ru>
2024-07-16 17:07:48 +0300

    Back-office_configuration_ServiceEmailsConfiguration

---
+++ back-office/src/main/java/telran/blocker/configuration/ServiceEmailsConfiguration.java
@@ -0,0 +1,29 @@
+package telran.blocker.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import telran.blocker.model.ServiceEmailsDoc;
+
+@RequiredArgsConstructor
+@Configuration
+@Getter
+public class ServiceEmailsConfiguration {
+
+	@SuppressWarnings("serial")
+	List<ServiceEmailsDoc> ConfigurationSEList = new ArrayList<>() {
+		
+		{
+			add(new ServiceEmailsDoc("web_A_service", new String[] {"hana2user@gmail.com", "hana2user+1@gmail.com" }));
+			add(new ServiceEmailsDoc("web_B_service", new String[] {"hana2user@gmail.com", "hana2user+2@gmail.com" }));)
+			add(new ServiceEmailsDoc("web_C_service", new String[] {"hana2user@gmail.com", "hana2user+3@gmail.com" }));)
+			add(new ServiceEmailsDoc("web_D_service", new String[] {"hana2user@gmail.com", "hana2user+4@gmail.com" }));)
+		
+		}
+	};
+	
+}
