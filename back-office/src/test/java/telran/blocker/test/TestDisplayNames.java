ae02fb0dc0ad1af94589b9640b66065af319a436
SimaKogan <simas96@mail.ru>
2024-07-17 15:10:21 +0300

    Back-office_blocker_test_TestDisplayNames

---
+++ back-office/src/test/java/telran/blocker/test/TestDisplayNames.java
@@ -0,0 +1,14 @@
+package telran.blocker.test;
+
+public interface TestDisplayNames {
+	
+	String ADD_IP_SUCCESS = "add IP normal";
+	String ADD_IP_EXCEPT = "add IP with exception";
+	String DELETE_IP_SUCCESS = "delete IP normal";
+	String DELETE_IP_EXCEPT = "delete IP with exception";
+	
+	String ADD_SERVICE_SUCCESS = "add SERVICE normal";
+	String AD_SERVICE_EXCEPT = "add SERVICE with exception";
+	String DELETE_SERVICE_SUCCESS = "delete SERVICE normal";
+	String DELETE_SERVICE_EXCEPT = "delete SERVICE with exception";
+}
