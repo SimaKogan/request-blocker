2c0d39bfe634b692ac7795b46e252b743be0f440
SimaKogan <simas96@mail.ru>
2024-07-17 13:19:53 +0300

    Back-office_model_ServiceEmailsDoc

---
+++ back-office/src/main/java/telran/blocker/model/ServiceEmailsDoc.java
@@ -0,0 +1,28 @@
+package telran.blocker.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import telran.blocker.dto.ServiceEmails;
+
+@Document(collection = "service-emails")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+public class ServiceEmailsDoc {
+	@Id
+	String webService;
+	String[] emails;
+	
+	public ServiceEmailsDoc(ServiceEmails serviceEmails) {
+		this.webService = serviceEmails.webService();
+		this.emails = serviceEmails.emails();
+		}
+	
+	public ServiceEmails build() {
+		return new ServiceEmails(webService, emails);
+	}
+}
