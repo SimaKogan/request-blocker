41dea6cf7e406a5f9c4e5de27943058354114864
SimaKogan <simas96@mail.ru>
2024-07-17 13:10:38 +0300

    Back-office_model_IpDataDoc

---
+++ back-office/src/main/java/telran/blocker/model/IpDataDoc.java
@@ -0,0 +1,52 @@
+package telran.blocker.model;
+
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.*;
+import telran.blocker.dto.IpData;
+
+
+@Document(collection = "blocking-list")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IpDataDoc {
+	@Id
+	String IP;
+	@Setter
+	String webService;
+	@Setter
+	long timestamp;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(IP, webService);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IpDataDoc other = (IpDataDoc) obj;
+		return Objects.equals(IP, other.IP) && Objects.equals(webService, other.webService);
+	}
+	
+	public IpDataDoc(IpData ipData) {
+		this.IP = ipData.IP();
+		this.webService = ipData.webService();
+		this.timestamp = System.currentTimeMillis();
+	}
+	
+	public IpData buildDto() {
+		return new IpData(IP, webService, timestamp);
+		
+	}
+
+}
