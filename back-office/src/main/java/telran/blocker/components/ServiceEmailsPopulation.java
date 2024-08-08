cb98360715e6b8c223d3598e186ac27cca20af68
SimaKogan <simas96@mail.ru>
2024-08-09 00:58:59 +0300

    Service_Emails_Population

--- back-office/src/main/java/telran/blocker/components/ServiceEmailsPopulation.java
+++ back-office/src/main/java/telran/blocker/components/ServiceEmailsPopulation.java
@@ -8,7 +8,9 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.blocker.configuration.ServiceEmailsConfiguration;
+import telran.blocker.model.IpDataDoc;
 import telran.blocker.model.ServiceEmailsDoc;
+import telran.blocker.repo.IpDataRepo;
 import telran.blocker.repo.ServiceEmailsRepo;
 
 @Component
@@ -17,23 +19,38 @@ import telran.blocker.repo.ServiceEmailsRepo;
 public class ServiceEmailsPopulation {
 
 	final ServiceEmailsRepo seRepo;
+	final IpDataRepo ipRepo;
 	final ServiceEmailsConfiguration configuration;
-	
+
 	@PostConstruct
-	void setUp( ) {
-		List<ServiceEmailsDoc> listAll = seRepo.findAll();
-		if (!listAll.isEmpty()) {
+	void setUp() {
+		List<ServiceEmailsDoc> listSE = seRepo.findAll();
+		if (!listSE.isEmpty()) {
 			log.debug("ServiceEmailsRepo is not empty");
 		} else {
 			populationSERepo();
-		}
-
+		};
+		
+		List<IpDataDoc> listIP = ipRepo.findAll();
+		if(!listIP.isEmpty()) {
+			log.debug("IpDataRepo is not empty");
+		} else {
+			populateIPRepo();
+		};
 	}
-	
+
+
 	public void populationSERepo() {
 		List<ServiceEmailsDoc> configurationSEList = configuration.getConfigurationSEList();
 		seRepo.saveAll(configurationSEList);
-		log.debug("Configuration service-email list has been saved to ServiceEmailRepo");
-	}
+		log.debug("Configuration service-emails list has been saved to ServiceEmailsRepo");
 
+	}
+	
+	private void populateIPRepo() {
+		List<IpDataDoc> configurationIPList = configuration.getConfigurationIPList();
+		ipRepo.saveAll(configurationIPList);
+		log.debug("Configuration IP list has been saved to IpDataRepo");
+		
+	}
 }
