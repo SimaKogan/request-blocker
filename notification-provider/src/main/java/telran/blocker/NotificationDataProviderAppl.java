38f94020c267f761a07df8a3f8b06dda4a617c41
SimaKogan <simas96@mail.ru>
2024-08-09 01:14:16 +0300

    Notification_Data_Provider_Appl

---
+++ notification-provider/src/main/java/telran/blocker/NotificationDataProviderAppl.java
@@ -0,0 +1,16 @@
+package telran.blocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "telran")
+public class NotificationDataProviderAppl {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationDataProviderAppl.class, args);
+
+	}
+
+}
