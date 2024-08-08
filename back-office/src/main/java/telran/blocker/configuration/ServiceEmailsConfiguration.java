79af9031af6f72e5b8adcd4ec5e8300c34c683ac
SimaKogan <simas96@mail.ru>
2024-08-09 00:54:46 +0300

    Service_Emails_Configuration

--- back-office/src/main/java/telran/blocker/configuration/ServiceEmailsConfiguration.java
+++ back-office/src/main/java/telran/blocker/configuration/ServiceEmailsConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import telran.blocker.model.IpDataDoc;
 import telran.blocker.model.ServiceEmailsDoc;
 
 @RequiredArgsConstructor
@@ -15,15 +16,20 @@ import telran.blocker.model.ServiceEmailsDoc;
 public class ServiceEmailsConfiguration {
 
 	@SuppressWarnings("serial")
-	List<ServiceEmailsDoc> ConfigurationSEList = new ArrayList<>() {
-		
+	List<ServiceEmailsDoc> configurationSEList = new ArrayList<>() {
+
 		{
-			add(new ServiceEmailsDoc("web_A_service", new String[] {"hana2user@gmail.com", "hana2user+1@gmail.com" }));
-			add(new ServiceEmailsDoc("web_B_service", new String[] {"hana2user@gmail.com", "hana2user+2@gmail.com" }));
-			add(new ServiceEmailsDoc("web_C_service", new String[] {"hana2user@gmail.com", "hana2user+3@gmail.com" }));
-			add(new ServiceEmailsDoc("web_D_service", new String[] {"hana2user@gmail.com", "hana2user+4@gmail.com" }));
-		
+			add(new ServiceEmailsDoc("web_A_service", new String[] { "hana2user@gmail.com", "hana2user+1@gmail.com" }));
+			add(new ServiceEmailsDoc("web_B_service", new String[] { "hana2user@gmail.com", "hana2user+2@gmail.com" }));
+			add(new ServiceEmailsDoc("web_C_service", new String[] { "hana2user@gmail.com", "hana2user+3@gmail.com" }));
+			add(new ServiceEmailsDoc("web_D_service", new String[] { "hana2user@gmail.com", "hana2user+4@gmail.com" }));
+
 		}
 	};
-	
+
+	List<IpDataDoc> configurationIPList = List.of(new IpDataDoc("1.1.1.1", "service_T1", System.currentTimeMillis()),
+			new IpDataDoc("2.1.1.1", "service_T2", System.currentTimeMillis()),
+			new IpDataDoc("3.1.1.1", "service_T3", System.currentTimeMillis()),
+			new IpDataDoc("4.1.1.1", "service_T4", System.currentTimeMillis()));
+
 }
