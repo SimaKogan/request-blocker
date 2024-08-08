3b2553a259f6138995271ac7c7800a9c0d5df503
SimaKogan <simas96@mail.ru>
2024-08-09 00:43:45 +0300

    Analyzer_Controller_Test

--- access-analyzer/src/test/java/telran/blocker/AnalyzerControllerTest.java
+++ access-analyzer/src/test/java/telran/blocker/AnalyzerControllerTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -36,49 +35,50 @@ class AnalyzerControllerTest {
 	InputDestination producer;
 	@Autowired
 	OutputDestination consumer;
-	
+
 	@MockBean
 	AnalyzerService analyzerService;
-	@Value("${app.analyzer.bing.name:analyzerData-out-0}")
+	@Value("${app.analyzer.binding.name:analyzerData-out-0}")
 	String producerBindingName;
 	String consumerBindingName = "consumerAnalyzer-in-0";
-	
-	
-	
+
 	static final IpData NO_BLOCK_IP_DATA = new IpData("100.100.100.100", "WEBname1", 1000);
 	static final IpData BLOCK_IP_DATA = new IpData("200.200.200.200", "name", 0);
 	static final String ipBlocked = "200.200.200.200";
 	static final IpData IP_DATA_WITH_BLOCKED_IP = new IpData(ipBlocked, "name", 0);
 	
+	@Value("${app.analyzer.max.list.size}")
+	int num_sending;
+
 	@BeforeEach
 	void setUp() {
 		when(analyzerService.getList(BLOCK_IP_DATA))
-		.thenReturn(List.of(BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA));
+				.thenReturn(List.of(BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA, BLOCK_IP_DATA));
 		when(analyzerService.getList(NO_BLOCK_IP_DATA)).thenReturn(null);
 	}
 
-	
-	
 	@Test
 	@Order(1)
 	void testIpNoBlock() {
 		producer.send(new GenericMessage<>(NO_BLOCK_IP_DATA), consumerBindingName);
 		Message<byte[]> message = consumer.receive(10, producerBindingName);
-		if(message != null) {
+		if (message != null) {
 			log.debug("message: {}", message.toString());
 		}
 		assertNull(message);
 	}
-	
+
 	@Test
 	@Order(2)
 	void testIpBlocked() throws Exception {
 		producer.send(new GenericMessage<IpData>(BLOCK_IP_DATA), consumerBindingName);
-		Message<byte[]> message = consumer.receive(10, producerBindingName);
-		assertNotNull(message);
-		ObjectMapper mapper = new ObjectMapper();
-		IpData actual = mapper.readValue(message.getPayload(), IpData.class);
-		assertEquals(IP_DATA_WITH_BLOCKED_IP, actual);
+		for(int i = 0; i < num_sending; i++) {
+			Message<byte[]> message = consumer.receive(10, producerBindingName);
+			assertNotNull(message);
+			ObjectMapper mapper = new ObjectMapper();
+			IpData actual = mapper.readValue(message.getPayload(), IpData.class);
+			assertEquals(IP_DATA_WITH_BLOCKED_IP, actual);
+		};
 	}
 
 }
